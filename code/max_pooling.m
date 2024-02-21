5ea753314d97ef4246a6f4b7ddc9ed30437f2476
Yixuan Li <68525696+Physics-Lee@users.noreply.github.com>
2024-02-21 20:33:17 +0800

    add annotations

--- code/max_pooling.m
+++ code/max_pooling.m
@@ -6,8 +6,8 @@ num_groups = floor(n / frame_per_volume);
 intensity_reshaped = reshape(intensity(1:num_groups*frame_per_volume), frame_per_volume, []);
 
 % Calculate max, omitnan
-intensity_volume = intensity_reshaped(2,:);
-% intensity_volume = max(intensity_reshaped, [], 1);
+% intensity_volume = intensity_reshaped(2,:); % for Liu Qi's con-focal
+intensity_volume = max(intensity_reshaped, [], 1);
 intensity_volume = intensity_volume';
 
 end
